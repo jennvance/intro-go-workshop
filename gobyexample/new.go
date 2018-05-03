5d309f0cace2219bf60990c45173b54d3b837264
jennvance <vancejenniferm@gmail.com>
2018-05-03 14:06:07 -0600

    more go

--- gobyexample/new.go
+++ gobyexample/new.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 )
 
-func plus(a int, b int) int {
-	return a + b
-}
-
-func plusPlus(a, b, c int) int {
-	return a + b + c
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
 }
 
 func main() {
-	res := plus(1, 2)
-	fmt.Println("1+2 =", res)
+	sum(1, 2)
+	sum(1, 2, 3)
 
-	res = plusPlus(1, 2, 3)
-	fmt.Println("1+2+3=", res)
+	nums := []int{1, 2, 3, 4}
+	sum(nums...)
 }
