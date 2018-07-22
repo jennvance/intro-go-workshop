e52c717ce9f1c0b171d0c159147e7694bc70446e
jennvance <vancejenniferm@gmail.com>
2018-07-21 20:58:42 -0400

    recursion

--- gobyexample/new.go
+++ gobyexample/new.go
@@ -4,23 +4,13 @@ import (
 	"fmt"
 )
 
-func zeroval(ival int) {
-	ival = 0
-}
-
-func zeroptr(iptr *int) {
-	*iptr = 0
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
 }
 
 func main() {
-	i := 1
-	fmt.Println("initial:", i)
-
-	zeroval(i)
-	fmt.Println("zeroval:", i)
-
-	zeroptr(&i)
-	fmt.Println("zeroptr:", i)
-
-	fmt.Println("pointer:", &i)
+	fmt.Println(fact(7))
 }
