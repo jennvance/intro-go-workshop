3616f287b82695ad3c46c638efe4e1536919d9c1
jennvance <vancejenniferm@gmail.com>
2018-07-22 22:19:28 -0400

    closures

--- gobyexample/new.go
+++ gobyexample/new.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 )
 
-func fact(n int) int {
-	if n == 0 {
-		return 1
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
 	}
-	return n * fact(n-1)
 }
 
 func main() {
-	fmt.Println(fact(7))
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
 }
